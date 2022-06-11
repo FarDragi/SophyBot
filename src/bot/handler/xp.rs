5e7206ccac24f941bae9cb99b0dae3c0c966ba5e
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-06-10 22:19:43 -0300

    Teste cache

--- src/bot/handler/xp.rs
+++ src/bot/handler/xp.rs
@@ -1,7 +1,12 @@
+use chrono::Utc;
 use poise::serenity_prelude::{Guild, User};
 
-use crate::{bot::state::State, error::AppError};
+use crate::{bot::state::State, cache::user::UserCache, error::AppError};
 
 pub async fn add_xp(user: &User, guild: &Guild, state: &State) -> Result<(), AppError> {
+    let now = Utc::now().timestamp();
+
+    let last_update = state.cache.get_user_last_message(user.id.0).await;
+
     Ok(())
 }
